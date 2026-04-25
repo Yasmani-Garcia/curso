3171ca95b80c59c87aff8fcf0e89c58b1a1daa76
Yasmani-Garcia <148160942+Yasmani-Garcia@users.noreply.github.com>
2026-04-25 14:30:37 -0500

    Actualizado python002.py

--- python002.py
+++ python002.py
@@ -1,10 +1,13 @@
 #variable en Python
-edad_usuario = 29 #enteros
+#numeros enteros
+edad_usuario = 29 
+numero decimal
 estatura_usuario = 1.69 #decimales
 #Cadena de caracteres
 nombre_usuario = 'Alex' #string
 casado_usuario = True
 discapacidad_usuario = False
+#Salida por consola
 
 print("Usuario: "+nombre_usuario,"\nEdad:",edad_usuario,"\nEstatura:  ",estatura_usuario,"\nCasado: ",casado_usuario,"\nDiscapcidad: ",discapacidad_usuario)
 
