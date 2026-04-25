42bdf56ebe232192f942430158295002e7d33cc4
Yasmani-Garcia <hgarcia1535@utm.edu.ec>
2026-04-25 14:26:55 -0500

    Mi segundo ejercicio en Python

---
+++ python002.py
@@ -0,0 +1,10 @@
+#variable en Python
+edad_usuario = 29 #enteros
+estatura_usuario = 1.69 #decimales
+#Cadena de caracteres
+nombre_usuario = 'Alex' #string
+casado_usuario = True
+discapacidad_usuario = False
+
+print("Usuario: "+nombre_usuario,"\nEdad:",edad_usuario,"\nEstatura:  ",estatura_usuario,"\nCasado: ",casado_usuario,"\nDiscapcidad: ",discapacidad_usuario)
+
